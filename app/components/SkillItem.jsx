518aed61479fed0344cd73f6b7a458b58bc3aeb1
yoohan-dex <yao460733480@outlook.com>
2016-07-06 19:41:41 +0800

    try to come out

--- app/components/SkillItem.jsx
+++ app/components/SkillItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 const SkillItem = (props) => {
   const item = props.item;
@@ -6,10 +6,13 @@ const SkillItem = (props) => {
     <dd className="skill-description-item">{desc}</dd>;
   return (
     <dl className="skill-description">
-      <dt className="skill-brief">{props.item.brief}</dt>
-      {props.item.descriptions.map(renderDescription)}
+      <dt className="skill-brief">{item.brief}</dt>
+      {item.descriptions.map(renderDescription)}
     </dl>
   );
 };
 
+SkillItem.propTypes = {
+  item: PropTypes.object,
+};
 export default SkillItem;
