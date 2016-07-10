1458722bb2f613043c4924f017defc75309d32b1
yoohan-dex <yao460733480@outlook.com>
2016-07-10 15:43:48 +0800

    ..

--- app/components/Skill.jsx
+++ app/components/Skill.jsx
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 import Section from './Section';
 import SkillItem from './SkillItem';
 const Skill = props => {
-  const skill = props.skill;
+  const { skill } = props;
   const renderItem = item =>
     <SkillItem item={item} />;
   return (
