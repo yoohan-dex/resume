39e7bb419e0aed1e80d990caaf3acce9a8ba9133
yoohan-dex <yao460733480@outlook.com>
2016-08-04 15:49:00 +0800

    honor optional

--- app/components/Education.jsx
+++ app/components/Education.jsx
@@ -5,12 +5,13 @@ const Education = props => {
   const section = props.section_names.education;
   const renderHonor = (honor) =>
     <dd className="education-honor">{honor}</dd>;
+  const honers = education.honor ? education.honor.map(renderHonor) : null;
   return (
     <Section startTime={education.start} endTime={education.end} section={section}>
       <dl className="education-description">
         <dt className="education-school">{education.school}</dt>
         <dd className="education-major">{education.major}</dd>
-        {education.honor.map(renderHonor)}
+        {honers}
       </dl>
     </Section>
   );
